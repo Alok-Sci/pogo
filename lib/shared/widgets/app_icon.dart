6b935e59ff9a7e0d05cd675e8ab9313d2930aad4
Alok-Sci <alokscientist7905@gmail.com>
2026-09-08 18:33:34 +0530

    feat(icons): create a wrapper class for svg icons

---
+++ lib/shared/widgets/app_icon.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class AppIcon extends StatelessWidget {
+  const AppIcon(
+    this.assetPath, {
+    this.size,
+    this.blendMode = BlendMode.srcIn,
+    super.key,
+  });
+
+  final String assetPath;
+  final double? size;
+  final BlendMode blendMode;
+
+  @override
+  Widget build(BuildContext context) {
+    final double defaultSize = size ?? 24;
+
+    return RepaintBoundary(
+      child: SvgPicture.asset(
+        assetPath,
+        width: defaultSize,
+        height: defaultSize,
+        placeholderBuilder: (context) => SizedBox(
+          height: defaultSize,
+          width: defaultSize,
+        ),
+      ),
+    );
+  }
+}
