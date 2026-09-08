2408736d6496a947403a4f50cf0dcdd7c6a9bdbf
Alok-Sci <alokscientist7905@gmail.com>
2026-09-08 16:20:06 +0530

    feat(assets): add asset path constants

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,14 @@
+abstract class AppIcons {
+  AppIcons._();
+
+  static const String _base = 'assets/icons';
+
+  static const String arrowDown = '$_base/arrow-down.svg';
+  static const String heart = '$_base/heart.svg';
+  static const String loader = '$_base/loader.svg';
+  static const String logo = '$_base/logomark.svg';
+  static const String paginationArrows = '$_base/pagination_dots.svg';
+  static const String playCicle = '$_base/play-cricle.svg';
+  static const String search = '$_base/search-normal.svg';
+  static const String star = '$_base/star.svg';
+}
---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,16 @@
+abstract class AppImages {
+  AppImages._();
+
+  static const String _base = 'assets/images';
+
+  static const String avatar = '$_base/avatar.png';
+  static const String connectWithCoach = '$_base/connect_with_coach.png';
+  static const String dietPlan = '$_base/diet_plan.png';
+  static const String foodBowl = '$_base/food_bowl.png';
+  static const String fruitSpliceGroup = '$_base/fruit_splice_group.png';
+  static const String homeHeaderBanner = '$_base/home_header.png';
+  static const String locationPin = '$_base/location_pin.png';
+  static const String meal = '$_base/meal.png';
+  static const String mockupOnboarding = '$_base/mockup_onboarding.png';
+  static const String wallet = '$_base/wallet.png';
+}
