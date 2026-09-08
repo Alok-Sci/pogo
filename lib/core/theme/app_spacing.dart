fa5ad4f2b6a8b2836183659224b4bd5a0979f947
Alok-Sci <alokscientist7905@gmail.com>
2026-09-08 15:51:28 +0530

    feat(theme): extract common spacings from figma

---
+++ lib/core/theme/app_spacing.dart
@@ -0,0 +1,18 @@
+abstract class AppSpacing {
+  AppSpacing._();
+
+  static const double xs2 = 3;
+  static const double xs = 4;
+  static const double sm = 6;
+  static const double smMd = 7;
+  static const double md = 8;
+  static const double lg = 10;
+  static const double lgMd = 12;
+  static const double xl = 14;
+  static const double xlMd = 15;
+  static const double xxl = 16;
+  static const double xxlMd = 18;
+  static const double xxxl = 20;
+  static const double huge = 30;
+  static const double massive = 39;
+}
