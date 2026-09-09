e67199226b21ac842e1b34d4ca0bf4fd23210ef3
Alok-Sci <alokscientist7905@gmail.com>
2026-09-09 16:22:58 +0530

    feat(assets): export google icon and add constant

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -6,6 +6,7 @@ abstract class AppIcons {
   static const String arrowDown = '$_base/arrow-down.svg';
   static const String heart = '$_base/heart.svg';
   static const String logo = '$_base/logomark.svg';
+  static const String googleLogo = '$_base/google_logo.svg';
   static const String paginationArrows = '$_base/pagination_dots.svg';
   static const String playCicle = '$_base/play-cricle.svg';
   static const String search = '$_base/search-normal.svg';
