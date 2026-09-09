bb14193288e319adf860fb56f3e94e17d1a823d3
Alok-Sci <alokscientist7905@gmail.com>
2026-09-09 13:40:17 +0530

    feat(widgets): add a common text button widget

---
+++ lib/shared/widgets/primary_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:pogo/core/constants/app_radii.dart';
+import 'package:pogo/core/theme/app_palette.dart';
+import 'package:pogo/core/theme/app_spacing.dart';
+import 'package:pogo/core/theme/app_text_styles.dart';
+import 'package:pogo/core/utils/double_extensions.dart';
+
+class PrimaryButton extends StatelessWidget {
+  const PrimaryButton(
+    this.text, {
+    super.key,
+    required this.onPressed,
+    this.backgroundColor = Palette.green,
+    this.foregroundColor = Palette.warmOffWhite,
+    this.padding = const EdgeInsets.symmetric(
+      horizontal: AppSpacing.lgMd,
+      vertical: AppSpacing.smMd,
+    ),
+  });
+
+  final String text;
+  final EdgeInsets? padding;
+  final Color? backgroundColor;
+  final Color? foregroundColor;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(
+        backgroundColor: backgroundColor,
+        foregroundColor: foregroundColor,
+        elevation: 0,
+        padding: padding,
+        textStyle: AppTextStyles.titleMedium,
+        shape: RoundedRectangleBorder(
+          borderRadius: AppRadii.full.circular,
+        ),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      ),
+      child: Text(text),
+    );
+  }
+}
