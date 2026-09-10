338edecaadf42efb3061593fe72c47ce8dc0755f
Alok-Sci <alokscientist7905@gmail.com>
2026-09-11 00:44:07 +0530

    feat(core): introduce toShortString extension to num

--- lib/core/utils/num_extensions.dart
+++ lib/core/utils/num_extensions.dart
@@ -5,3 +5,12 @@ extension GapExtensions on num {
   Widget get vGap => SizedBox(height: toDouble());
   Widget get hGap => SizedBox(width: toDouble());
 }
+
+extension ShortNumberStringExtensions on num {
+  String get toShortString {
+    if (this < 1000) return toStringAsFixed(0);
+    if (this < 1000000) return '${(this / 1000).toStringAsFixed(1)}K';
+    if (this < 1000000000) return '${(this / 1000000).toStringAsFixed(1)}M';
+    return '${(this / 1000000000).toStringAsFixed(1)}B';
+  }
+}
