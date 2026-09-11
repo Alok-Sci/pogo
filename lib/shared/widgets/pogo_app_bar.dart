2572dc2ca3cf0db45ae39a513aa1d4ac773a1c93
Alok-Sci <alokscientist7905@gmail.com>
2026-09-11 14:00:51 +0530

    feat(widgets): add support for infoText in PogoAppBar

--- lib/shared/widgets/pogo_app_bar.dart
+++ lib/shared/widgets/pogo_app_bar.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pogo/core/constants/app_icons.dart';
+import 'package:pogo/core/constants/app_radii.dart';
 import 'package:pogo/core/theme/app_palette.dart';
 import 'package:pogo/core/theme/app_text_styles.dart';
+import 'package:pogo/core/utils/context_extensions.dart';
+import 'package:pogo/core/utils/double_extensions.dart';
 import 'package:pogo/shared/widgets/circular_icon_button.dart';
 
 class PogoAppBar extends StatelessWidget implements PreferredSizeWidget {
   const PogoAppBar({
     super.key,
     required this.title,
+    this.infoText,
   });
   final String title;
+  final String? infoText;
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize =>
+      Size.fromHeight(kToolbarHeight + (infoText != null ? 37 : 0));
 
   @override
-  AppBar build(BuildContext context) {
-    return AppBar(
+  Widget build(BuildContext context) {
+    final PreferredSizeWidget appBar = AppBar(
+      backgroundColor: Colors.transparent,
       centerTitle: true,
       title: Text(
         title,
@@ -42,5 +49,39 @@ class PogoAppBar extends StatelessWidget implements PreferredSizeWidget {
         ),
       ),
     );
+
+    return infoText == null
+        ? appBar
+        : PreferredSize(
+            preferredSize: preferredSize,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: context.appColors.headerInvertedGradient,
+                borderRadius: AppRadii.m.bottomCircular,
+                backgroundBlendMode: BlendMode.colorBurn,
+              ),
+              child: Column(
+                children: [
+                  appBar,
+                  Divider(
+                    height: 0,
+                    color: Palette.charcoalGrey.withValues(alpha: .3),
+                  ),
+                  SizedBox(
+                    height: 37,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          infoText!,
+                          style: AppTextStyles.titleItalic12,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
   }
 }
